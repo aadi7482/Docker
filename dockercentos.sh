64677ba5a0eb3dbc4136d519f694020345fd4777
Shivam Kumar Pandey <107880189+aadi7482@users.noreply.github.com>
2024-12-18 14:53:03 +0530

    Create dockercentos.sh

---
+++ dockercentos.sh
@@ -0,0 +1,7 @@
+sudo yum check-update
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
